0da4d3aaa1a95ceb794ce554c0a481c680137f35
singo8068 <singo8068@yahoo.co.jp>
2026-07-26 04:07:27 +0900

    Add files via upload

--- ousamaigo/server.js
+++ ousamaigo/server.js
@@ -1,13 +1,58 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'matiai.html'));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "matiai.html"));
 });
 
-app.use(express.static('public'));
+let rooms = [];
+
+io.on("connection", (socket) => {
+
+    // 接続したら募集一覧を送る
+    socket.emit("roomList", rooms);
+
+    // 募集する
+    socket.on("createRoom", room => {
+        room.id = socket.id;
+        rooms.push(room);
+        io.emit("roomList", rooms);
+    });
+
+    // 参加する
+    socket.on("joinRoom", id => {
+
+        const room = rooms.find(r => r.id === id);
+
+        if (!room) return;
+
+        socket.join(id);
+        io.sockets.sockets.get(id)?.join(id);
+
+        io.to(id).emit("startGame", room);
+
+        rooms = rooms.filter(r => r.id !== id);
+        io.emit("roomList", rooms);
+    });
+
+    // 切断
+    socket.on("disconnect", () => {
+        rooms = rooms.filter(r => r.id !== socket.id);
+        io.emit("roomList", rooms);
+    });
+
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+
+server.listen(PORT, () => {
+    console.log("Server Start");
+});
