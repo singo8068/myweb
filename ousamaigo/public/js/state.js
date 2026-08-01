d335635db096779252d8bc6ed93d8d9e755ae611
singo8068 <singo8068@yahoo.co.jp>
2026-08-01 10:57:03 +0900

    Add files via upload

--- ousamaigo/public/js/state.js
+++ ousamaigo/public/js/state.js
@@ -72,10 +72,11 @@ let blackYasumi=0;
 let whiteYasumi=0;
 let kaesisu=0;
 let pawatorisu=0;
+let uemsg="";
 if(ISNET){
- if(myColor==="black"){let uemsg="じぶんのばんだよ";
- }else{let uemsg="あいてのばんだよ";
+ if(myColor==="black"){uemsg="じぶんのばんだよ";
+ }else{uemsg="あいてのばんだよ";
  }
 }else{
-let uemsg="のばんだよ";
+ uemsg="のばんだよ";
 }
