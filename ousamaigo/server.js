31c9124009e60c1ff3caa24db5dfccb15455959f
singo8068 <singo8068@yahoo.co.jp>
2026-08-05 20:02:22 +0900

    Add files via upload

--- ousamaigo/server.js
+++ ousamaigo/server.js
@@ -18,7 +18,18 @@ app.get("/", (req, res) => {
 
 
 let rooms = [];
+function sendGameData(socket, room,eventName, data) {
+    if (!room) return;
 
+    // 今後ここで
+    // ・持ち時間更新
+    // ・ACK管理
+    // ・ログ
+    // ・再送管理
+    // などを追加
+
+    socket.to(data.roomId).emit(eventName, data);
+}
 io.on("connection", (socket) => {
 console.log("connect:", socket.id);
     // 接続したら募集一覧を送る
@@ -28,6 +39,10 @@ console.log("connect:", socket.id);
     socket.on("createRoom", room => {
         room.roomId = socket.id;
         room.hostId = socket.id;
+
+room.blackTime = 60000;
+room.whiteTime = 60000;
+room.turn = "black";
         rooms.push(room);
         io.emit("roomList", rooms);
     });
@@ -49,6 +64,7 @@ const hostColor = Math.random() < 0.5 ? "black" : "white";
 const guestColor = hostColor === "black" ? "white" : "black";
 
 // それぞれに違う情報を送る
+room.lastUpdate=Date.now()+ 2000;
 hostSocket?.emit("startGame", {
     roomId: room.roomId,
     size: room.size,
@@ -79,23 +95,21 @@ socket.on("joinGameRoom", roomId => {
     socket.join(roomId);
     console.log("game join", socket.id, roomId);
 });
-//ねこうち
-socket.on("putStone", data => {
-    socket.to(data.roomId).emit("putStone", data);
-});
-//ためる
-socket.on("tameru", data => {
-    socket.to(data.roomId).emit("tameru", data);
-});
-//パワーうち
-socket.on("pawa", data => {
-    socket.to(data.roomId).emit("pawa", data);
-});
-//リバース
-socket.on("reverse", data => {
-    socket.to(data.roomId).emit("reverse", data);
+const gameEvents = [
+    "putStone",
+    "tameru",
+    "pawa",
+    "reverse"
+];
+gameEvents.forEach(eventName => {
+    socket.on(eventName, data => {
+const room = rooms.find(r => r.roomId === data.roomId);
+        sendGameData(socket, room,eventName, data);
+    });
 });
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 
